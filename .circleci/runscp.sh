832dc2c871cb106831e520962ad3f8f214756416
Mark Potts <Mark.Potts@noaa.gov>
2021-04-27 18:28:16 -0400

    update for build of gfortran_9.3.0_openmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_05-6-gb8c1a70 on chianti

--- .circleci/runscp.sh
+++ .circleci/runscp.sh
@@ -8,19 +8,31 @@ cd ~/esmf-test-artifacts
 git config --global user.email "mark.potts@noaa.gov"
 git config --global user.name "dcv-bot"
 git branch
-export hash=`git for-each-ref --sort=-committerdate refs/heads/ | head -n 1 | awk -F" " '{print $1}'`
+export hash=${CIRCLE_SHA1}
 export TERM=xterm
 export host=${CIRCLE_BRANCH}
 export message=`git log --format=%B -n 1 $hash | head -n 1`
-export branch=`echo $message |  awk -F " " '{print $4}' | awk -F"_._" '{print $2}'`
+export branch=`echo $message |  awk -F ' ' '{print $5}' | awk -F '_._' '{print $2}'`
+export bmessage=`echo $message |  awk -F ' ' '{print $8}'` 
 export branch=${branch:-'develop'}
-export branchhash=`echo $message |  awk -F " " '{print $8}' | awk -F"-" '{print $3}'`
+export branchhash=`echo $message |  awk -F ' ' '{print $8}' | awk -F '-' '{print $3}'`
+echo $hash
 echo $host
 echo $branch
 echo $message
-git cherry-pick $hash
-git cherry-pick --continue
-find $branch -iname summary.dat | xargs grep -l "hash = $branchhash" | xargs grep -L "hash = $branchhash." | xargs grep "test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > "$branch/$branchhash.summary"
+which column
+git checkout $hash $branch/$host
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "unit test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > unit
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "system test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > sys
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "example test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > examp
+paste -d " " unit sys examp > "$branch/$bmessage.summary"
 git add $branch
+git pull -X theirs --no-edit origin main
 git commit -a -m"$message"
 git push origin main
+while [ $? -ne 0 ]
+do
+  git pull -X theirs --no-edit origin main
+  git push origin main
+done
+
