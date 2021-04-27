afe83158c5cd47b512ba4bc276a942bfa09a1721
Mark A Potts <mark.potts@noaa.gov>
2021-04-27 09:38:04 -0400

    updated runscp in circleci [ci skip]

--- .circleci/runscp.sh
+++ .circleci/runscp.sh
@@ -19,8 +19,7 @@ echo $hash
 echo $host
 echo $branch
 echo $message
-git cherry-pick $hash
-git cherry-pick --continue
+git checkout $hash $branch/$host
 find $branch -iname summary.dat | xargs grep -l "hash = $branchhash" | xargs grep -L "hash = $branchhash." | xargs grep "test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > "$branch/$branchhash.summary"
 git add $branch
 git commit -a -m"$message"
