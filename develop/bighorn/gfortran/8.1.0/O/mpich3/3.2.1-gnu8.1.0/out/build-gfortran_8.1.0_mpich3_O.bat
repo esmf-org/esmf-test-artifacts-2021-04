9323eeab06bb92920fa82c9f7267c676b829771c
Mark Potts <mpotts@bighorn.cgd.ucar.edu>
2021-04-28 11:36:47 -0600

    update for build of gfortran_8.1.0_mpich3_O_develop with hash ESMF_8_2_0_beta_snapshot_05-6-gb8c1a70 on bighorn

---
+++ develop/bighorn/gfortran/8.1.0/O/mpich3/3.2.1-gnu8.1.0/out/build-gfortran_8.1.0_mpich3_O.bat
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+export JOBID=12345
+
+module use /project/esmf/modulefiles
+module load compiler/gnu/8.1.0 mpich/3.2.1-gnu8.1.0 tool/netcdf/4.6.1/gcc-8.1.0
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/mpotts/gfortran_8.1.0_mpich3_O_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpich3
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 8 clean 2>&1| tee clean_$JOBID.log 
+make -j 8 2>&1| tee build_$JOBID.log
+
