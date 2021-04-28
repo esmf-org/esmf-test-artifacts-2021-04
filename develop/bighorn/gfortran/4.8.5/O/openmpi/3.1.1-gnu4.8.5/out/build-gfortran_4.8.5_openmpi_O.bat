4c1da32967c561ad23f0ff438663d6a7de457186
Mark Potts <mpotts@bighorn.cgd.ucar.edu>
2021-04-28 10:56:37 -0600

    update for build of gfortran_4.8.5_openmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_05-6-gb8c1a70 on bighorn

---
+++ develop/bighorn/gfortran/4.8.5/O/openmpi/3.1.1-gnu4.8.5/out/build-gfortran_4.8.5_openmpi_O.bat
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+export JOBID=12345
+
+module use /project/esmf/modulefiles
+module load compiler/gnu/4.8.5 openmpi/3.1.1-gnu4.8.5 tool/netcdf/4.6.1/gcc-4.8.5
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/mpotts/gfortran_4.8.5_openmpi_O_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 8 clean 2>&1| tee clean_$JOBID.log 
+make -j 8 2>&1| tee build_$JOBID.log
+
