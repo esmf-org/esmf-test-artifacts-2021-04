4d802bef19a997ad944cd909efcdfbe8b4706534
Mark Potts <mpotts@bighorn.cgd.ucar.edu>
2021-04-28 14:07:47 -0600

    update for build of nag_7.0_mpiuni_O_develop with hash ESMF_8_2_0_beta_snapshot_05-6-gb8c1a70 on bighorn

---
+++ develop/bighorn/nag/7.0/O/mpiuni/out/build-nag_7.0_mpiuni_O.bat
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+export JOBID=12345
+
+module use /project/esmf/modulefiles
+export ESMF_MPIRUN=/scratch/mpotts/nag_7.0_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
+module load compiler/nag/7.0  tool/netcdf/4.6.1/nag
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/mpotts/nag_7.0_mpiuni_O_develop
+export ESMF_COMPILER=nag
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 8 clean 2>&1| tee clean_$JOBID.log 
+make -j 8 2>&1| tee build_$JOBID.log
+
