8c87b99b142141b238025e48749e56768fe1186a
Mark Potts <mpotts@bighorn.cgd.ucar.edu>
2021-04-28 12:57:19 -0600

    update for build of gfortran_8.1.0_mvapich2_O_develop with hash ESMF_8_2_0_beta_snapshot_05-6-gb8c1a70 on bighorn

---
+++ develop/bighorn/gfortran/8.1.0/O/mvapich2/mvapich2-2.3rc2-qlc/out/test-gfortran_8.1.0_mvapich2_O.bat
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+export JOBID=12346
+
+module use /project/esmf/modulefiles
+module load compiler/gnu/8.1.0 mpi/gcc/mvapich2-2.3rc2-qlc tool/netcdf/4.6.1/gcc-8.1.0
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/mpotts/gfortran_8.1.0_mvapich2_O_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mvapich2
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
