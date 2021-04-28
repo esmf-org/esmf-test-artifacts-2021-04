da38c28faa175fc98fb0e432889f5459a8549ad8
Mark Potts <mpotts@bighorn.cgd.ucar.edu>
2021-04-28 16:31:44 -0600

    update for build of nag_6.2_mvapich2_O_develop with hash ESMF_8_2_0_beta_snapshot_05-6-gb8c1a70 on bighorn

---
+++ develop/bighorn/nag/6.2/O/mvapich2/mvapich2-2.3rc2/out/test-nag_6.2_mvapich2_O.bat
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+export JOBID=12346
+
+module use /project/esmf/modulefiles
+module load compiler/nag/6.2 mpi/nag/mvapich2-2.3rc2 tool/netcdf/4.6.1/nag
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/mpotts/nag_6.2_mvapich2_O_develop
+export ESMF_COMPILER=nag
+export ESMF_COMM=mvapich2
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
